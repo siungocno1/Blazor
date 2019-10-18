02937fb38a5665d1087981907a71512e8cf1eef3
Phuong Nguyen <Sjungok@outlook.com>
2019-10-18 17:51:00 +0700

    Add project files.

---
+++ CEDTeam.App/Data/User.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CEDTeam.App.Data
+{
+    public class UserService
+    {
+        public async Task<string> GetUser()
+        {
+            var a = new List<dynamic>
+            {
+                new {name = "Phuong", age = 23},
+                new {name = "Ngan", age = 23}
+            };
+            return JsonConvert.SerializeObject(a);
+        }
+    }
+}
---
+++ CEDTeam.App/wwwroot/js/app.js
@@ -0,0 +1,29 @@
+﻿var cedteam = {};
+(function () {
+    cedteam.alert = {
+        option: {
+            timeout: 1000,
+            message: "This is a message"
+        },
+        init: function (option) {
+            this.option = option;
+        },
+        show: function (t) {
+            alert(this.option.message);
+            t && setTimeout(function () {
+                alert(2);
+            }, t);
+        },
+        hide: function (e) {
+
+        }
+    }
+    cedteam.user = {
+        show: function (a) {
+            var json = JSON.parse(a);
+            $(json).each(function (i, e) {
+                $("#user").append("<p>"+e.name+"-"+e.age+"</p>");
+            });
+        }
+    }
+})();
